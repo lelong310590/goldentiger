4916f44d9ad019fe7246ef2668229fb6c987441b
Train Heartnet <longlengoc90@gmail.com>
2023-09-25 11:56:24 +0700

    update fonts

--- resources/views/themes/deepblack/layouts/user.blade.php
+++ resources/views/themes/deepblack/layouts/user.blade.php
@@ -38,6 +38,7 @@
     <!--[if lt IE 9]>
     <script type="application/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script type="application/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
     <![endif]-->
 </head>
 

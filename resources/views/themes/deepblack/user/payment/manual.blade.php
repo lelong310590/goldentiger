8d5d32e1c357b4f124058027538e1c6eb886dcbf
Train Heartnet <longlengoc90@gmail.com>
2023-10-10 13:38:07 +0700

    change wallet qr

--- resources/views/themes/deepblack/user/payment/manual.blade.php
+++ resources/views/themes/deepblack/user/payment/manual.blade.php
@@ -18,7 +18,7 @@
                 <div class="col-md-12">
                     <div class="card secbg br-4">
                         <div class="card-body">
-                            <div class="row ">
+                            <div class="row">
                                 <div class="col-md-12">
                                     <h3 class="title text-center">{{trans('Please follow the instruction below')}}</h3>
                                     <p class="text-center mt-2 ">{{trans('You have requested to deposit')}}  <b class="text--base">{{getAmount($order->amount)}}
@@ -29,8 +29,8 @@
                                     <p class="mt-2 ">
                                         <p style="text-align: center; ">Please send <b>USDT-BEP20</b> to this address with exactly your total deposit amount:</p>
                                         <p style="text-align: center; margin-top: 10px" class="mt-2">
-                                            <img src="https://api.qrserver.com/v1/create-qr-code/?color=fae15e&amp;bgcolor=222222&amp;data=0x7Ef6bDc6FF3B20830a5Ad7DE496B603C06569Dc9&amp;qzone=1&amp;margin=0&amp;size=200x200&amp;ecc=L" style="width: 200px;"><br></p>
-                                            <p style="text-align: center; " class="mt-2">0x7Ef6bDc6FF3B20830a5Ad7DE496B603C06569Dc9</p>
+                                            <img src="https://api.qrserver.com/v1/create-qr-code/?color=fae15e&amp;bgcolor=222222&amp;data={{auth()->user()->wallet_address}}&amp;qzone=1&amp;margin=0&amp;size=200x200&amp;ecc=L" style="width: 200px;"><br></p>
+                                            <p style="text-align: center; " class="mt-2">{{auth()->user()->wallet_address}}</p>
                                         <p style="text-align: center; margin-top: 10px;">After your transaction confirmed successfully please provide these following info:</p>
                                         <?php //echo optional($order->gateway)->note; ?>
                                     </p>
